262c8ca598a10f913ccb2e5f7d8648952f91eb4f
Max Walker <maxwalker23@gmail.com>
2025-07-14 16:48:29 -0400

    Debugs

--- index.js
+++ index.js
@@ -571,7 +571,7 @@ app.post("/api/submit-application", async (req, res) => {
     const browser = await puppeteer.launch({
       args: chromium.args,
       defaultViewport: chromium.defaultViewport,
-      executablePath: chromium.executablePath,
+      executablePath: await chromium.executablePath(),
       headless: chromium.headless,
     });
     const page = await browser.newPage();
